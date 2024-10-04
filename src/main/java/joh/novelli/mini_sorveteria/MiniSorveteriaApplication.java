89ddc50d86d25d5f47cec2600528c9f483f0d1e0
johnovelli <joh.novelli90@gmail.com>
2024-10-04 10:39:59 -0300

    Mini-Soverteria - Estrutura básica

---
+++ src/main/java/joh/novelli/mini_sorveteria/MiniSorveteriaApplication.java
@@ -0,0 +1,13 @@
+package joh.novelli.mini_sorveteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniSorveteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniSorveteriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/joh/novelli/mini_sorveteria/MiniSorveteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package joh.novelli.mini_sorveteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniSorveteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
